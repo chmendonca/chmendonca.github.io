d36c3d29ca251dd35fdd77d6a40ddf7ac6724e90
chmendonca <chmendonca1977@gmail.com>
2020-06-28 11:40:45 -0300

    Finalizadas as alterações nas páginas de Java

--- js/functions.js
+++ js/functions.js
@@ -3,5 +3,5 @@ function mudaFoto(foto){
     document.getElementById("picture").src=foto;
 }
 function atualizaData(){
-    document.getElementById("updatedDate").innerHTML="Site Last Update: 2020, June 27<sup>th</sup>";
+    document.getElementById("updatedDate").innerHTML="Site Last Update: 2020, June 28<sup>th</sup>";
 }
