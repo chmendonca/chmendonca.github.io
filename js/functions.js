99e312332f51032ab1751e0607e90ea16e48296e
chmendonca <chmendonca1977@gmail.com>
2020-07-20 21:53:08 -0300

    updated the date of last modification

--- js/functions.js
+++ js/functions.js
@@ -3,5 +3,5 @@ function mudaFoto(foto){
     document.getElementById("picture").src=foto;
 }
 function atualizaData(){
-    document.getElementById("updatedDate").innerHTML="Site Last Update: 2020, July 16<sup>th</sup>";
+    document.getElementById("updatedDate").innerHTML="Site Last Update: 2020, July 20<sup>th</sup>";
 }
