30697ef00b40f04cce3a91ddcbfce6c8ebd9ab6f
chmendonca <chmendonca1977@gmail.com>
2020-07-22 20:05:18 -0300

    Added a hint for Python/Kivy and updated the modification date

--- js/functions.js
+++ js/functions.js
@@ -3,5 +3,5 @@ function mudaFoto(foto){
     document.getElementById("picture").src=foto;
 }
 function atualizaData(){
-    document.getElementById("updatedDate").innerHTML="Site Last Update: 2020, July 20<sup>th</sup>";
+    document.getElementById("updatedDate").innerHTML="Site Last Update: 2020, July 22<sup>nd</sup>";
 }
